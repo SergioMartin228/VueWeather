2be6b8360c3fea496ccc197ef97788d7eb5bb2bf
Sergey Chernishev <schern329@gmail.com>
2020-12-17 21:49:41 +0300

    Add working divided weather card

---
+++ src/components/Container.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="informer-container">
+        <Temperature/>
+        <div class="flex-break"></div>
+        <Humidity/>
+        <div class="vertical-divider"></div>
+        <Wind/>
+    </div>
+</template>
+<script>
+import Temperature from './Temperature.vue'
+import Humidity from './Humidity.vue'
+import Wind from './Wind.vue'
+export default {
+    name: "Container",
+    components:{
+        Temperature,
+        Humidity,
+        Wind
+    }
+}
+</script>
+
+<style>
+.informer-container {
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  width: 400px;
+  padding: 30px;
+  background-image: url('/img/forest.png');
+  background-size: cover;
+  background-position: center center;
+  border-radius: 20px;
+  box-shadow: 0px 0px 10px 0px rgba(50, 50, 50, 0.75);
+}
+.flex-break{
+  flex-basis: 100%;
+  height: 0;
+}
+
+.vertical-divider{
+  height: 70px;
+  width: 1px;
+  background-color: #fff;
+  vertical-align: middle;
+  display: table-cell;
+}
+</style>
